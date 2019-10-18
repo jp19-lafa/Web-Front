985d1fb7139666dcc626f0e6c702753c204583ac
JonasVanHoof <jonas.vanhoof@outlook.com>
2019-10-18 10:54:31 +0200

    Squashed commit of the following:
    commit 16010deb9ea6b613b71335423c20878c2c339177
    Author: MattZK <me@mattwill.be>
    Date:   Fri Oct 18 10:47:44 2019 +0200
    
        Added a navigation bar
    
    commit f5e5c5df1c7e4a942a327c91cb10f229d556686e
    Author: MattZK <me@mattwill.be>
    Date:   Fri Oct 18 10:44:36 2019 +0200
    
        Added auth base code
    
    commit 2a9e2458f3ac64c4c4dfe75256d4a6edd5603079
    Author: MattZK <me@mattwill.be>
    Date:   Fri Oct 18 10:43:38 2019 +0200
    
        Added reset stylesheet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PublicComponent } from './pages/public/public.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'public', component: PublicComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
+//// Modules ////
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthenticationModule } from './authentication/authentication.module';
 
+//// Routing Module ////
 import { AppRoutingModule } from './app-routing.module';
+
+//// Components ////
 import { AppComponent } from './app.component';
 import { PublicComponent } from './pages/public/public.component';
 import { PublicNavigationComponent } from './pages/public-navigation/public-navigation.component';
+import { NavigationComponent } from './global/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PublicComponent,
-    PublicNavigationComponent
+    PublicNavigationComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AuthenticationModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
