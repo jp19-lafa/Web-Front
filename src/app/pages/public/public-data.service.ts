55cca25189f32c436941211c1f00fcf961bf90a3
JonasVanHoof <jonas.vanhoof@outlook.com>
2019-10-18 15:41:19 +0200

    Show clicked farm When clicked on the farm the title will be shown below.

--- src/app/pages/public/public-data.service.ts
+++ src/app/pages/public/public-data.service.ts
@@ -24,6 +24,7 @@ export class PublicDataService {
       info: 'Node located in the city of dummy.Educational use.',
     },
   ]
+  activePage: Node = this.nodes[0];
   constructor() { }
 }
 export interface Node {
--- src/app/pages/public/public.component.ts
+++ src/app/pages/public/public.component.ts
@@ -14,5 +14,4 @@ export class PublicComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
