f5e5c5df1c7e4a942a327c91cb10f229d556686e
MattZK <me@mattwill.be>
2019-10-18 10:44:36 +0200

    Added auth base code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
+//// Modules ////
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthenticationModule } from './authentication/authentication.module';
 
+//// Routing Module ////
 import { AppRoutingModule } from './app-routing.module';
+
+//// Components ////
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AuthenticationModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
