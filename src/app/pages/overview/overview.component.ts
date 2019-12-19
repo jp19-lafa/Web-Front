005c6d1b12d08949161d4e1b347cba8fc98b439f
jonasvanhoof <jonasvanhoof@outlook.com>
2019-12-19 10:07:16 +0100

    Add top buttons manage, modes

--- src/app/pages/overview/overview.component.ts
+++ src/app/pages/overview/overview.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { NodeDataService } from 'src/app/providers/API/node-data.service';
 import { Node, LineGraphConfig, IODeviceType } from 'src/app/providers/interfaces';
 import { ActivatedRoute } from '@angular/router';
+import { faWindowRestore, faBuilding } from '@fortawesome/free-regular-svg-icons';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
+
 import { tick } from '@angular/core/testing';
 
 @Component({
@@ -10,7 +13,9 @@ import { tick } from '@angular/core/testing';
   styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent implements OnInit {
-
+  public faWindowRestore = faWindowRestore;
+  public faBuilding = faBuilding;
+  public faCog = faCog;
   nodes: Node[] = [];
   activeNode: Node;
 
